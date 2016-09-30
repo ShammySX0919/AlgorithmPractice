e0fafb8093ccefca91bf681a622c025ded6dc140
Andrew Ma <ama@bitstew.com>
2016-09-30 14:56:39 -0700

    make LIS easier to read. also added .project for eclipse

--- src/dp/LIS.java
+++ src/dp/LIS.java
@@ -1,5 +1,8 @@
 package dp;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by andrew on 30/09/16.
  */
@@ -29,11 +32,12 @@ public class LIS {
             dpLis[i] = 1;
         }
         //from bottom up, no duplicate calculation as compared with top down approach
-        for (int u = 1; u < arr.length; u++) {
-            for (int d = 0; d < u; d++) {
+        for (int upBound = 1; upBound < arr.length; upBound++) {
+            for (int bottomBound = 0; bottomBound < upBound; bottomBound++) {
                 //increase when condition satisfied
-                if (arr[u] > arr[d] && dpLis[u] < dpLis[d] + 1) {
-                    dpLis[u] = dpLis[d] + 1;
+            	//this also guarantees the dpLis[upBound] is max of all dpLis[bottomBound]+1
+                if (arr[upBound] > arr[bottomBound] && dpLis[upBound] < dpLis[bottomBound] + 1) {
+                    dpLis[upBound] = dpLis[bottomBound] + 1;
                 }
             }
         }
@@ -46,41 +50,44 @@ public class LIS {
     }
 
     /**
-     * under help of binary search for each number to determine it's positon in
+     * under help of binary search for each number to determine it's position in
      * a list that keeps updating. very good thought!
      */
     private static void lisNlgN(int[] nums) {
         if (nums == null || nums.length == 0)
             return;
-        //list can be natually an array
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        //list can be naturally an array, but I found using list is easier to understand
+        ArrayList<Integer> auxList = new ArrayList<Integer>();
 
         for (int num : nums) {
             //adding the num to tail of list if list is empty or num is greater than current tail of list
-            if (list.size() == 0 || num > list.get(list.size() - 1)) {
-                list.add(num);
+            if (auxList.size() == 0 || num > auxList.get(auxList.size() - 1)) {
+                auxList.add(num);
             } else {
                 //if number is smaller than current tail of list, try to find a position to insert it to list
-                int i = 0; //left boundary
-                int j = list.size() - 1;//right boundary. these two make the length of current list
-                //do binary search to determine position of num in the list
+                int left = 0; //left boundary
+                int right = auxList.size() - 1;//right boundary. these two make the length of current list
+                //do binary search to determine position of current num in the list
                 //you can also use java's binary search method on collections to return expected position
-                while (i < j) {
-                    int mid = (i + j) / 2;
-                    if (list.get(mid) < num) {
-                        i = mid + 1;
+                //the position returned indicates the current num is less than the one already there
+                //overwriting the existing one with this smaller one
+                //this is also an algorithm of using binary search to determine insertion position of a given number
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if (auxList.get(mid) < num) {
+                        left = mid + 1;
                     } else {
-                        j = mid;
+                        right = mid;
                     }
                 }
-                //after this, the j is num's expected postion in existing list
-                //overwrite the value in that postion: good! it's smaller than the value being overwriten!
-                //so to keep the sequence effect for all rest of numbers
-                list.set(j, num);
+                //after this, the right is num's expected position in existing list
+                //overwrite the value in that position: it's smaller than the value being overwritten!
+                //indices in auxList does not reflects a list of longest increasing sequence, but its size is
+                //the correct number of LIS
+                auxList.set(right, num);
             }
         }
 
-        //return list.size();
-        System.out.println(list.size());
+        System.out.println(auxList.size());
     }
 }

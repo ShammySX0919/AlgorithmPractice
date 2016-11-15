6dae6ba39f392c0f581463583f334ea489d5d9f5
Andrew Ma <ama@bitstew.com>
2016-11-15 15:00:41 -0800

    leetcode easy 453

---
+++ src/leetcode/Easy453_MinimumMovesToEqualArrayElements.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class Easy453_MinimumMovesToEqualArrayElements {
+	 public int minMoves(int[] nums) {
+	        int min=nums[0],sum=0;
+	        for(int i=0;i<nums.length;i++){
+	            sum+=nums[i];
+	            if(min>nums[i])
+	                {min=nums[i];}
+	        }
+	        return sum-min*nums.length;
+	    }
+}
---
+++ src/leetcode/Easy458_PoorPigs.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class Easy458_PoorPigs {
+	public static int poorPigs(int buckets, int minutesToDie,int minutesToTest){
+		return (int)(Math.ceil(Math.log10(minutesToTest/minutesToDie+1)/Math.log10(buckets)));
+	}
+}

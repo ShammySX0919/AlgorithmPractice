0094569d9810a5f7b02de4c9d7c93c7faa00da6e
Andrew Ma <ama@bitstew.com>
2017-05-02 09:59:59 -0700

    leetcode 568, fix bug in maxVacationDays. now it is working

--- src/leetcode/hard/MaximumVacation.java
+++ src/leetcode/hard/MaximumVacation.java
@@ -47,6 +47,9 @@ public class MaximumVacation {
         int cities = flightBetweenCities.length;
         int dp[][] = new int[weeks][cities];
         int maxVac = 0;
+        for(int week=0;week<weeks;week++){
+            Arrays.fill(dp[week], -1);
+        }
         //set up first week values for each city as start point
           for(int city = 0;city<cities;city++) {
               if (city == 0 || flightBetweenCities[0][city] == 1) {
@@ -57,10 +60,10 @@ public class MaximumVacation {
           //for each week, probe option for each city
         for(int week=1;week<weeks;week++){
             for(int city=0;city<cities;city++){
-
                 for(int tCity=0;tCity<cities;tCity++){//tCity is city's next level neighbor
                     //for the cities it can stay in the checking week. either stay in same city or flying to another city
-                    if((tCity==city||flightBetweenCities[city][tCity]==1)){
+                    //only reach to week-tCity if week-1--city is already processed
+                    if(dp[week-1][city]>-1 &&(tCity==city||flightBetweenCities[city][tCity]==1)){
                         //reachable city will now no longer negative
                         dp[week][tCity] = Math.max(dp[week][tCity],dp[week-1][city]+cityWeeklyVacation[tCity][week]);
                         maxVac = Math.max(maxVac,dp[week][tCity]);
@@ -123,13 +126,15 @@ public int maxVacationDaysBackward(int[][] flights, int[][] days) {
         int[][] cityVac = null;
         cityVac = new int[][]{{7,0,0},{0,7,0},{0,0,7}};
         System.out.println(MaximumVacation.maxVacationDays1D(cityConnections,cityVac));
-
+        System.out.println(MaximumVacation.maxVacationDays(cityConnections,cityVac));
         cityConnections = new int[][]{{0,0,0},{0,0,0},{0,0,0}};
         cityVac = new int[][]{{1,1,1},{7,7,7},{7,7,7}};
         System.out.println(MaximumVacation.maxVacationDays1D(cityConnections,cityVac));
+        System.out.println(MaximumVacation.maxVacationDays(cityConnections,cityVac));
         cityConnections = new int[][]{{0,1,1},{1,0,1},{1,1,0}};
         cityVac = new int[][]{{1,3,1},{6,0,3},{3,3,3}};
         System.out.println(MaximumVacation.maxVacationDays1D(cityConnections,cityVac));
+        System.out.println(MaximumVacation.maxVacationDays(cityConnections,cityVac));
     }
 
 }

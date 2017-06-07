e82544aaaf915ff302068169951257e3764e5395
Andrew Ma <comebacktj@yahoo.com>
2017-06-07 00:33:45 -0700

    more comments

--- src/amazon/hard/LeetCodeHard84_LargestRectangleInHistogram.java
+++ src/amazon/hard/LeetCodeHard84_LargestRectangleInHistogram.java
@@ -4,6 +4,19 @@ import java.util.Stack;
 
 /**
  * http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
+ * For every bar ‘x’, we calculate the area with ‘x’ as the smallest bar in the rectangle.
+ * If we calculate such area for every bar ‘x’ and find the maximum of all areas, our task is done.
+ * How to calculate area with ‘x’ as smallest bar? We need to know index of the first smaller (smaller than ‘x’) bar
+ * on left of ‘x’ and index of first smaller bar on right of ‘x’.
+ * Let us call these indexes as ‘left index’ and ‘right index’ respectively.
+ * --->brute force is following this idea, but expensive way to figure out left index and right index
+ * We traverse all bars from left to right, maintain a stack of bars. Every bar is pushed to stack once.
+ * A bar is popped from stack when a bar of smaller height is seen.
+ * When a bar is popped, we calculate the area with the popped bar as smallest bar.
+ *
+ * How do we get left and right indexes of the popped bar
+ * – the current index tells us the ‘right index’ and index of previous item in stack is the ‘left index’.
+ * -this holds well when equal height bars are together. a bigger area will be calculated out in next loop
  * Created by andrew on 6/6/2017.
  */
 public class LeetCodeHard84_LargestRectangleInHistogram {
